4808a7cfe9600b3f5768309c1ddbd3c57dcd8753
David-Galan-Cardona <david.galan.7e7@itb.cat>
2024-04-03 12:50:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace M03UF5AC1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Intro = "Introdueix 10 scores", Player = "Introdueix el nom del jugador", Mission = "Introdueix la missió", 
+                Scoring = "Introdueix la puntuació", Ranking = "El ranking de jugadors és: ";
+            List<Score> scores = new List<Score>();
+            string player, mission;
+            int scoring;
+                Console.WriteLine(Intro);
+            while (scores.Count < 10)
+            {
+                Console.WriteLine(Player);
+                player = Console.ReadLine() ?? "";
+                Console.WriteLine(Mission);
+                mission = Console.ReadLine() ?? "";
+                Console.WriteLine(Scoring);
+                scoring = int.Parse(Console.ReadLine());
+                if (Score.ComprovaPlayer(player) && Score.ComprovaMission(mission) && Score.ComprovaScoring(scoring))
+                {
+                    scores.Add(new Score(player, mission, scoring));
+                }
+            }
+            //test values
+            /*scores.Add(new Score("Laura", "Alfa-001", 120));
+            scores.Add(new Score("Hugo", "Alfa-002", 150));
+            scores.Add(new Score("Laura", "Alfa-001", 135));
+            scores.Add(new Score("Tomas", "Alfa-001", 200));
+            scores.Add(new Score("Laura", "Alfa-002", 175));*/
+            Console.WriteLine(Ranking);
+
+            foreach (Score s in Score.generateUniqueRanking(scores))
+            {
+                Console.WriteLine(s.ToString());
+            }
+        }
+    }
+}
---
+++ Score.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace M03UF5AC1
+{
+    public class Score
+    {
+        private string player;
+        private string mission;
+        private int scoring;
+
+        public void SetPlayer(string player)
+        {
+            if(ComprovaPlayer(player))
+            {
+                this.player = player;
+            }
+            else
+            {
+                Console.WriteLine("Error: El nom del jugador no és correcte");
+            }
+        }
+        public static bool ComprovaPlayer(string cadena)
+        {
+            return Regex.IsMatch(cadena, @"^[a-zA-Z]+$");
+        }
+        public string GetPlayer()
+        {
+            return this.player;
+        }
+
+        public void SetMission(string mission)
+        {
+            if(ComprovaMission(mission))
+            {
+                this.mission = mission;
+            }
+            else
+            {
+                Console.WriteLine("Error: El nom de la missió no és correcta");
+            }
+        }
+        public string GetMission()
+        {
+            return this.mission;
+        }
+        public static bool ComprovaMission(string cadena)
+        {
+            return Regex.IsMatch(cadena, @"^(Alfa|Beta|Gamma|Delta|Epsilon|Zeta|Eta|Theta|Iota|Kappa|Lambda|Mu|Nu|Xi|Omicron|Pi|Rho|Sigma|Tau|Upsilon|Phi|Chi|Psi|Omega)-[0-9]{3}$");
+        }
+
+        public void SetScoring(int scoring)
+        {
+            if(ComprovaScoring(scoring))
+            {
+                this.scoring = scoring;
+            }
+            else
+            {
+                Console.WriteLine("Error: La puntuació no és correcta");
+            }
+        }
+        public int GetScoring()
+        {
+            return this.scoring;
+        }
+        public static bool ComprovaScoring(int valor)
+        {
+            return valor >= 0 && valor <= 500;
+        }
+
+        public Score(string player, string mission, int scoring)
+        {
+            SetPlayer(player);
+            SetMission(mission);
+            SetScoring(scoring);
+        }
+        public override string ToString()
+        {
+            return "Player: " + GetPlayer() + " Mission: " + GetMission() + " Scoring: " + GetScoring();
+        }
+        public static List<Score> generateUniqueRanking(List<Score> scoreList)
+        {
+            List<Score> uniqueRanking = new List<Score>();
+            List<string> missions = new List<string>();
+            List<string> players = new List<string>();
+            foreach (Score s in scoreList)
+            {
+                if (!missions.Contains(s.GetMission()))
+                {
+                    missions.Add(s.GetMission());
+                }
+                if (!players.Contains(s.GetPlayer()))
+                {
+                    players.Add(s.GetPlayer());
+                }
+            }
+            foreach (string player in players)
+            {
+                foreach (string mission in missions)
+                {
+                    int max = 0;
+                    foreach (Score score in scoreList)
+                    {
+                        if (score.GetPlayer() == player && score.GetMission() == mission && score.GetScoring() > max)
+                        {
+                            max = score.GetScoring();
+                        }
+                    }
+                    if (max != 0)
+                    {
+                        uniqueRanking.Add(new Score(player, mission, max));
+                    }
+                }
+            }
+            uniqueRanking = uniqueRanking.OrderByDescending(o => o.GetScoring()).ToList();
+            return uniqueRanking;
+        }
+    }
+}
